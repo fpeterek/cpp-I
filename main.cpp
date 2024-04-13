b92f889ad7b95d56398c96e336e7321ea504e5f1
Filip Peterek <filip.peterek@fr24.com>
2024-04-13 12:51:09 +0200

    Custom tuple implementation

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <stdexcept>
 #include <tuple>
 
+#include "tuple.hpp"
+
 
 template<typename T, std::enable_if_t<std::is_same_v<T, int>, bool> = true>
 int load_item(const std::string& str) {
@@ -43,9 +45,9 @@ void parse_item(auto& tuple, std::stringstream& ss) {
     parse_item<idx+1, Ts...>(tuple, ss);
 }
 
-template<typename... T>
-std::tuple<T...> parse(const std::string& str) {
-    std::tuple<T...> tuple;
+template<template<typename...> typename TupleType, typename... T>
+TupleType<T...> parse(const std::string& str) {
+    TupleType<T...> tuple;
     std::stringstream ss { str };
 
     parse_item<0, T...>(tuple, ss);
@@ -59,13 +61,22 @@ void printTuple([[maybe_unused]] const std::tuple<>& tuple, [[maybe_unused]] std
 
 int main() {
 
-    auto tuple = parse<int, double, std::string, bool>("666,6.9,pseudosacrosanct perversion,true");
+    auto tuple = parse<std::tuple, int, double, std::string, bool>("666,6.9,pseudosacrosanct perversion,true");
     std::get<0>(tuple);
 
     std::cout
+        << std::boolalpha
         << std::get<0>(tuple) << ", "
         << std::get<1>(tuple) << ", "
         << std::get<2>(tuple) << ", "
         << std::get<3>(tuple) << std::endl;
+
+    auto parsed = parse<fp::tuple, int, double, std::string, bool>("123,4.56,string,false");
+
+    fp::tuple manuallyConstructed { 1, 2.4, std::string("str"), false };
+
+    fp::tuple<int, std::string, bool> empty;
+    std::cout << parsed << std::endl;
+    std::cout << manuallyConstructed << std::endl;
 
 }
---
+++ tuple.hpp
@@ -0,0 +1,98 @@
+#include <ostream>
+#include <utility>
+
+
+namespace fp {
+
+namespace {
+
+template<int idx, typename... Ts>
+struct tuple_impl {
+
+    template<int x, int get_idx, typename... get_ts, bool>
+    friend auto get_impl(tuple_impl<idx, Ts...>& t);
+
+    tuple_impl() = default;
+
+    void print(std::ostream& os) const;
+
+};
+
+template<int idx>
+struct tuple_impl<idx> {
+
+    tuple_impl() = default;
+
+    void print([[maybe_unused]] std::ostream& os) const { }
+};
+
+template<int idx, typename T, typename... Ts>
+struct tuple_impl<idx, T, Ts...> : public tuple_impl<idx+1, Ts...> {
+
+    T item;
+
+    tuple_impl() = default;
+
+    tuple_impl(T&& item, Ts&&... args) :
+        tuple_impl<idx + 1, Ts...>(std::forward<Ts>(args)...),
+        item(item) { }
+
+    void print(std::ostream& os) const {
+        os << "[" << idx << "]: " << item;
+        
+        if constexpr (sizeof...(Ts) > 0) {
+            os << ", ";
+        }
+
+        tuple_impl<idx+1, Ts...>::print(os);
+    }
+
+};
+
+template<int idx, typename T, typename... Ts>
+T& get_impl(tuple_impl<idx, T, Ts...>& t) {
+    return t.item;
+}
+
+} // namespace
+
+template<typename... Ts>
+class tuple : protected tuple_impl<0, Ts...> {
+
+    void print(std::ostream& os) const {
+        os << "fp::tuple { ";
+        fp::tuple_impl<0, Ts...>::print(os);
+        os << " }";
+    }
+
+public:
+
+    template<int idx>
+    auto& get() {
+        tuple_impl<0, Ts...>& t = *this;
+        return get_impl<idx>(t);
+    }
+
+    tuple() = default;
+    tuple(Ts&&... args) : tuple_impl<0, Ts...>(std::forward<Ts>(args)...) { }
+
+    template<typename... Args>
+    friend std::ostream& operator<<(std::ostream& os, const tuple<Args...>& tuple);
+};
+
+template<typename... Ts>
+std::ostream& operator<<(std::ostream& os, const tuple<Ts...>& tuple) {
+    tuple.print(os);
+    return os;
+}
+
+
+} // namespace fp
+
+
+namespace std {
+    template<int idx, typename... Ts>
+    auto& get(fp::tuple<Ts...>& tuple) {
+        return tuple.template get<idx>();
+    }
+}
